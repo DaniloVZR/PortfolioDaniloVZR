51fb96b1a7eb13da450c1bb00e2ff03d775226e6
Danilo Urrego <daniloud2809@gmail.com>
2024-07-01 19:28:16 -0500

    Styles: About Me section

--- components/About.tsx
+++ components/About.tsx
@@ -1,3 +1,4 @@
+import { textAboutMe } from "@/constants";
 import Image from "next/image";
 
 const About = () => {
@@ -5,14 +6,27 @@ const About = () => {
     <section id="about" className="pt-10 border-t-2 border-slate-800">
       <h1 className="heading tracking-normal w-auto p-10">About<span className="text-purple-primary">{' '}Me</span></h1>
       <div className="flex flex-col md:flex-row justify-center gap-16 md:gap-8 items-center py-[5rem] border-b-2 border-slate-800">
-        <div className="flex-1 text-lg font-normal space-y-5">
-          <p>🤠 I am a passionate and committed Frontend Web Developer seeking my first job as a developer.</p>
-          <p>🧠 Currently, I am focused on my training through the SoyHenry programming bootcamp, oriented towards web development, where I am acquiring valuable skills in the latest technologies and trends in the field, as well as soft skills.</p>
-          <p>🌟 I also have one year of experience in the IT sector as a Systems Assistant, which has provided me with a solid technical foundation. Additionally, I hold a professional degree in Information Systems Programming from Politécnico Jaime Isaza Cadavid and have a B2 English certificate with 600 hours of study at Comfama.</p>
-          <p>😉 I consider myself to have valuable soft skills for web development, such as the ability to work in a team, solve problems, learn quickly, and pay attention to detail. I am also a proactive, adaptable person with a strong interest in collaborating with organizational goals and ensuring excellence in every project.</p>
+        <div className="flex-1 relative">
+          <div className="relative text-lg font-normal space-y-5 z-10 cursor-default">
+            {textAboutMe.map((paragraph, index) => (
+              <div key={index} className="flex justify-center items-center z-10 relative">
+                <span
+                  className="mr-5 p-4 rounded-full text-2xl ease-in-out hover:scale-125 duration-300 "
+                  style={{
+                    background: "rgb(0, 0, 0, 0.5)"
+                  }}>{paragraph.emoji}</span>
+                <p>{paragraph.text}</p>
+              </div>
+            ))}
+          </div>
+          <div className="absolute z-0 left-[-50%] top-0 w-[70%] h-[60%] rounded-full purple__gradient" />
+          <div className="absolute z-0 left-[-50%] bottom-0 w-[70%] h-[60%] rounded-full white__gradient" />
         </div>
+
         <div className="hidden md:flex flex-1 justify-center items-center">
-          <div className="rounded-full overflow-hidden shadow-2xl" style={{ width: 300, height: 300, backgroundColor: 'rgba(0,0,0,0.5)' }}>
+          <div
+            className="rounded-full overflow-hidden shadow-2xl ease-in-out hover:scale-110 duration-300"
+            style={{ width: 300, height: 300, backgroundColor: 'rgba(0,0,0,0.5)' }}>
             <Image
               src="/Profile-Picture.png"
               alt="Profile picture"
--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,27 @@
+import { text } from "stream/consumers";
+
 export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
   { name: "Experience", link: "#experience" },
   { name: "Contact", link: "#contact" },
 ];
+
+export const textAboutMe = [
+  {
+    emoji: "🤠",
+    text: "I am a passionate and committed Frontend Web Developer seeking my first job as a developer."
+  },
+  {
+    emoji: "🧠",
+    text: "Currently, I am focused on my training through the SoyHenry programming bootcamp, oriented towards web development, where I am acquiring valuable skills in the latest technologies and trends in the field, as well as soft skills."
+  },
+  {
+    emoji: "🌟",
+    text: "I also have one year of experience in the IT sector as a Systems Assistant, which has provided me with a solid technical foundation. Additionally, I hold a professional degree in Information Systems Programming from Politécnico Jaime Isaza Cadavid and have a B2 English certificate with 600 hours of study at Comfama."
+  },
+  {
+    emoji: "😉",
+    text: "I consider myself to have valuable soft skills for web development, such as the ability to work in a team, solve problems, learn quickly, and pay attention to detail. I am also a proactive, adaptable person with a strong interest in collaborating with organizational goals and ensuring excellence in every project."
+  }
+]
