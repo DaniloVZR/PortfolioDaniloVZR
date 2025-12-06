67ec510b3f105daf98fc283acc0e4f28b9ebd36b
Danilo Urrego <daniloud2809@gmail.com>
2025-12-05 20:14:03 -0500

    Fix: Next Config

--- next.config.mjs
+++ next.config.mjs
@@ -5,16 +5,10 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
+    qualities: [75, 85, 90, 100],
   },
-  // Habilitar compresión
   compress: true,
-  // Optimizar producción
   swcMinify: true,
-  // Optimizar CSS
-  experimental: {
-    optimizeCss: true,
-  },
-  // Configuración de headers para mejor caching
   async headers() {
     return [
       {
